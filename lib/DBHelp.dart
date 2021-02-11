01548d947485090247dcdde24dad9b2d2a5648e3
hannasss <68788894+hannasss@users.noreply.github.com>
2021-02-11 13:55:42 +0900

    Update DBHelp.dart

--- lib/DBHelp.dart
+++ lib/DBHelp.dart
@@ -3,47 +3,49 @@ import 'package:path/path.dart';
 import 'package:project_moonhwadiary/name.dart';
 
 
-final String tableName = 'diarys';
+final String tableName = 'diaries';
 
 class DBHelper {
-  var _db;
+
+  DBHelper._();
+  static final DBHelper _db = DBHelper._();
+  factory DBHelper() => _db;
+
+  static Database _database;
 
   Future<Database> get database async {
-    if (_db != null) return _db;
-    _db = openDatabase(
-        join(await getDatabasesPath(), 'diarys.db'),
+    if(_database != null) return _database;
 
-        onCreate: (db, version) {
-          return db.execute(
-            'CREATE TABLE diarys(_no integer PRIMARY KEY AUTIONCREMENT, dateTime TEXT, title TEXT, contents TEXT, feel integer)',
-          );
-        },
-        version: 1);
-    return _db;
+    _database = await initDB();
+    return _database;
   }
+
+  initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, 'MoonhwadiaryDB.db');
+
+    return await openDatabase(
+        path,
+        version: 1,
+        onCreate: (db, version) async {
+          await db.execute('CREATE TABLE $tableName(_no integer PRIMARY KEY AUTOINCREMENT, datetime TEXT, title TEXT, contents TEXT, feel integer)');
+        },
+        onUpgrade: (db, oldVersion, newVersion){}
+    );
+  }
+
+
   Future<void> insertDiary(Diary diary) async {
     final db = await database;
 
-    await db.insert(
+    var res = await db.insert(
       tableName,
       diary.toMap(),
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
-  }
-  Future<List<Diary>> diarys() async {
-    final db = await database;
 
-    final List<Map<String, dynamic>> maps = await db.query('diarys');
+    print("insertDiary : " + res.toString());
 
-    // List<Map<String, dynamic>를 List<diary>으로 변환합니다.
-    return List.generate(maps.length, (i) {
-      return Diary(
-        title: maps[i]['title'],
-        contents: maps[i]['contents'] ,
-        dateTime: maps[i]['dateTime'],
-        feel: maps[i]['feel'],
-      );
-    });
   }
 
 
@@ -51,46 +53,73 @@ class DBHelper {
     final db = await database;
 
     // 주어진 Diary를 수정합니다.
-    await db.update(
+    var res = await db.update(
       tableName,
       diary.toMap(),
       // Diary의 dateTime가 일치하는 지 확인합니다.
-      where: "dateTime = ?",
+      where: "_no = ?",
       // Diary의 dateTime를 whereArg로 넘겨 SQL injection을 방지합니다.
-      whereArgs: [diary.dateTime],
+      whereArgs: [diary.no], //
     );
+
+    print("updateDiary : " + res.toString());
+
   }
 
-  Future<void> deleteDiary(DateTime dateTime) async {
+  Future<void> deleteDiary(int no) async {
     final db = await database;
 
     // 데이터베이스에서 Diary를 삭제합니다.
-    await db.delete(
+    var res = await db.delete(
       tableName,
       // 특정 diary를 제거하기 위해 `where` 절을 사용하세요
-      where: "dateTime = ?",
+      where: "_no = ?",
       // Diary의 dateTime를 where의 인자로 넘겨 SQL injection을 방지합니다.
-      whereArgs: [dateTime],
+      whereArgs: [no],
     );
+
+    print("deleteDiary : " + res.toString());
   }
 
-  Future<List<Diary>> findDiary(DateTime dateTime) async {
+  Future<List<Diary>> selectDiary(int no) async {
     final db = await database;
 
     // 모든 Diary를 얻기 위해 테이블에 질의합니다.
     final List<Map<String, dynamic>> maps =
-    await db.query('diarys', where: 'dateTime = ?', whereArgs: [dateTime]);
+    await db.query('diaries', where: 'no = ?', whereArgs: [no]);
+
+    print("selectDiary : "+ maps.toString());
 
     // List<Map<String, dynamic>를 List<Diary>으로 변환합니다.
     return List.generate(maps.length, (i) {
       return Diary(
         title: maps[i]['title'],
         contents: maps[i]['contents'] ,
-        dateTime: maps[i]['dateTime'],
+        dateTime: maps[i]['datetime'],
         feel: maps[i]['feel'],
       );
     });
   }
 
+  Future<List<Diary>> selectAllDiary() async {
+    final db = await database;
 
+    // 월별 검색
+    //final List<Map<String, dynamic>> maps = await db.query('diaries', where:"strftime('%m', datetime) = '01'");
+
+    // 전체 검색
+    final List<Map<String, dynamic>> maps = await db.query('diaries');
+
+    print("selecAlltDiary : "+ maps.toString());
+
+    // List<Map<String, dynamic>를 List<diary>으로 변환합니다.
+    return List.generate(maps.length, (i) {
+      return Diary(
+        title: maps[i]['title'],
+        contents: maps[i]['contents'],
+        dateTime: maps[i]['datetime'],
+        feel: maps[i]['feel'],
+      );
+    });
+  }
 }
